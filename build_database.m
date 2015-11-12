4bce3241bf74ff0feb1af0fcdd6552befbb2b3dc
Nathan Perkins <nathan@maxmo.net>
2015-11-12 11:49:13 -0500

    Store second-to-laster layer data. Ignore database directory.

--- build_database.m
+++ build_database.m
@@ -28,6 +28,9 @@ net_input_shape = net.blobs('data').shape;
 network_dim = net_input_shape(1);
 net.blobs('data').reshape([net_input_shape(1:(end - 1)) batch_size]);
 
+% second to last layer
+second_to_last_layer = net.blob_names{end-1};
+
 % get mean (prevent loading per frame)
 d = load('~/Development/caffe-master/matlab/+caffe/imagenet/ilsvrc_2012_mean.mat');
 im_mean = d.mean_data;
@@ -48,6 +51,8 @@ data_frame_ids = [];
 data_timestamps = [];
 data_features = single([]);
 data_distances = single([]);
+data_features2 = single([]);
+data_distances2 = single([]);
 
 % profile
 tm_frame = 0;
@@ -65,6 +70,7 @@ for video_id = 1:length(videos)
     
     % all scores (for all frames)
     all_scores = {};
+    all_scores2 = {};
     all_times = [];
     
     % process video in batches of frames
@@ -113,6 +119,8 @@ for video_id = 1:length(videos)
         output = net.forward(input);
         tm_network = tm_network + toc;
         
+        % SCORES FROM LAST LAYER
+        
         % scores
         scores = output{1};
         if cur_batch_size < batch_size
@@ -121,13 +129,26 @@ for video_id = 1:length(videos)
         
         % append to scores
         all_scores{end + 1} = scores; %#ok<SAGROW>
+        
+        % SCORES FROM SECOND TO LAST LAYER (pre-softmax)
+        
+        % get second to last layer scores
+        scores2 = net.blobs(second_to_last_layer).get_data();
+        if cur_batch_size < batch_size
+            scores2 = scores2(:, 1:cur_batch_size);
+        end
+        
+        % append to scores
+        all_scores2{end + 1} = scores2; %#ok<SAGROW>
     end
     
     % concatenate all scores together
     all_scores = cat(2, all_scores{:});
+    all_scores2 = cat(2, all_scores2{:});
     
     % get distances
     distances = sqrt(mean(diff(all_scores, 1, 2) .^ 2, 1));
+    distances2 = sqrt(mean(diff(all_scores2, 1, 2) .^ 2, 1));
     
     % append video id
     data_video_ids = [data_video_ids (video_id * ones(size(all_times)))]; %#ok<AGROW>
@@ -135,10 +156,12 @@ for video_id = 1:length(videos)
     data_timestamps = [data_timestamps all_times]; %#ok<AGROW>
     data_features = [data_features all_scores]; %#ok<AGROW>
     data_distances = [data_distances nan distances]; %#ok<AGROW>
+    data_features2 = [data_features2 all_scores2]; %#ok<AGROW>
+    data_distances2 = [data_distances2 nan distances2]; %#ok<AGROW>
 end
 
 % save
-save(database, 'data_video_ids', 'data_frame_ids', 'data_timestamps', 'data_features', 'data_distances');
+save(database, 'videos', 'data_video_ids', 'data_frame_ids', 'data_timestamps', 'data_features', 'data_distances', 'data_features2', 'data_distances2');
 
 %% CLEAN UP
 caffe.reset_all();
